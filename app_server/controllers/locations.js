674e7793918e6e0ded6a8d823c1d6b4e163e2996
Andy <aliedholm@gmail.com>
2017-05-05 18:49:42 -0400

    working on 7.3.2 made all changes but homepage does not display any locations

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,32 +1,40 @@
+var request = require('request');
+var apiOptions = {
+  server : "http://64.137.235.106:3000"
+};
+
+var renderHomepage = function(req, res, responseBody){
+	res.render('locations-list', {
+		title: 'Loc8r - find a place to work with wifi',
+		pageHeader: {
+			title: 'Loc8r',
+			strapline: 'Find places to work with wifi near you!'
+		},
+		sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+		locations: responseBody
+	});
+};
+
 /* GET home page */
 module.exports.homelist = function(req, res){
-  res.render('locations-list', { 
-    title: 'Loc8r - find a place to work with wifi',
-    pageHeader: {
-      title: 'Loc8r',
-      strapline: 'Fine places to work with wifi near you!'
-    },
-    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhads with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-    locations: [{
-      name: 'Starcups',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '100m'
-    },{
-      name: 'Cafe Hero',
-      address: '125 High Street, Reading RG6 1PS',
-      rating: 4,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '200m'
-    },{
-      name: 'Burger Queen',
-      address: '125 High Street, Reading RG6 1PS',
-      rating: 2,
-      facilities: ['Food', 'Premium wifi'],
-      distance: '250m'
-    }]
- });
+	var requestOptions, path;
+	path = '/api/locations';
+	requestOptions = {
+		url : apiOptions.server + path,
+		method : "GET",
+		json : {},
+		qs : {
+			lng : -0.7992599,
+			lat : 51.378091,
+			maxDistance : 20000000000
+		}
+	};
+	request(
+		requestOptions,
+		function(err, response, body) {
+			renderHomepage(req, res, body);
+		}
+		);
 };
 
 /* GET Location Info page */
